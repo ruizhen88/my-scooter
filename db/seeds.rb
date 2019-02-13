4697d1a732721048d2378f08ccd218fbe25c94b6
Kane Ryans <kaneryans@hotmail.co.uk>
2019-02-13 17:41:55 +0800

    completed seeds file to generate 1 entry for each db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,73 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+User.destroy_all
+Scooter.destroy_all
+Booking.destroy_all
+Review.destroy_all
+
+puts 'Creating database entries...'
+
+STATUS = ["Accepted", "Pending", "Cancelled", "Completed"]
+
+# *** Commented out for testing..eventually would like to loop and populate DBs with more information  ***
+# 10.times do
+#   users = User.new(
+#     first_name: Faker::Name.first_name.to_s,
+#     last_name: Faker::Name.last_name.to_s,
+#     email: Faker::Internet.email
+#   )
+#   users.save
+#   5.times do
+#     scooters = (
+#       make: Faker::Vehicle.make.to_s,
+#       model: Faker::Vehicle.model.to_s,
+#       year: rand(2010..2018),
+#       location: Faker::Address.city,
+#       registration_plate: "FA02 8JK",
+#       img: Faker::Placeholdit.image('50x50'),
+#       price: rand(10..20),
+#       owner: user
+#     )
+#   end
+# end
+
+user = User.new(
+    first_name: Faker::Name.first_name.to_s,
+    last_name: Faker::Name.last_name.to_s,
+    email: Faker::Internet.email
+    )
+user.save
+
+scooter = (
+      make: Faker::Vehicle.make.to_s,
+      model: Faker::Vehicle.model.to_s,
+      year: rand(2010..2018),
+      location: Faker::Address.city,
+      registration_plate: "FA02 8JK",
+      img: Faker::Placeholdit.image('50x50'),
+      price: rand(10..20),
+      owner: user
+      )
+scooter.save
+
+booking = Booking.new(
+    amount: rand(10..20),
+    scooter_id: ,
+    customer_id: ,
+    start_date: "01-02-2019",
+    end_date: "10-02-2019",
+    status: STATUS.sample
+  )
+booking.save
+
+review = Review.new(
+    booking_id: booking,
+    content: "some random string review",
+    rating: rand(1..5)
+  )
+review.save
+
+puts 'Finished!'
