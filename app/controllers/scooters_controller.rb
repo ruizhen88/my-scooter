70c2b34b3a34902759cca7aedd78a9005118db41
John Higgins <johnhiggins@outlook.ie>
2019-02-14 15:41:07 +0800

    added scooter routes, controllers and views

---
+++ app/controllers/scooters_controller.rb
@@ -0,0 +1,41 @@
+class ScootersController < ApplicationController
+  # All scooters show up on the first page
+  def index
+    @scooters = Scooter.all
+  end
+
+  def show
+    @scooter = Scooter.find(params[:id])
+  end
+
+  def new
+    @scooter = Scooter.new
+  end
+
+  def create
+    @owner = User.find(params[:id])
+    @scooter = Scooter.new(scooter_params)
+    @scooter.owner = @owner
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  # Only the owner can destroy his/her scooters at dashboard
+  # To Do: find out each ids
+  def destroy
+    @owner = User.find(params[:id])
+    @scooter = Scooter.find(params[:id])
+    @scooter.destroy
+    redirect_to owner_dashboarda
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+end
