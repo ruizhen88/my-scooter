44cc7d89e7b9188394ba7d45b1aa4464e0d32252
John Higgins <johnhiggins@outlook.ie>
2019-02-18 12:17:13 +0800

    created models and controllers for conversations and messages

--- config/routes.rb
+++ config/routes.rb
@@ -15,4 +15,8 @@ Rails.application.routes.draw do
   resources :users do
     resources :bookings, only: [ :index, :show ]
   end
+
+  resources :conversations do
+    resources :messages
+  end
 end
